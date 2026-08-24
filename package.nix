99f00505a6eead290ecac0de892f3a1b68cdc447
Ryan Bright <rbright@users.noreply.github.com>
2026-08-24 06:35:11 -0600

    chore(deps): update pi-agent to v0.84.3 (#87)
    Co-authored-by: rbright <168179+rbright@users.noreply.github.com>

--- package.nix
+++ package.nix
@@ -13,16 +13,16 @@
 }:
 buildNpmPackage (finalAttrs: {
   pname = "pi-agent";
-  version = "0.84.2";
+  version = "0.84.3";
 
   nodejs = nodejs_22;
 
   src = fetchzip {
     url = "https://github.com/earendil-works/pi/releases/download/v${finalAttrs.version}/pi-${finalAttrs.version}-source.tar.gz";
-    hash = "sha256-UJr6NAfjKM/xldjmyx4W28K9I8jJz/dh3vz6eLi1I40=";
+    hash = "sha256-x76ZO6zg08EFhf+1oxPLhJhDyNNn7s5EvyTPYERehNs=";
   };
 
-  npmDepsHash = "sha256-6J5Efe+6ptCuR3VZojwYPZO8BBnnZsOQ4OAeB64uYOY=";
+  npmDepsHash = "sha256-cDx28+c4bwtQpiy5+BCvZhZezoZb4WRqfZj2eoEeMbw=";
   npmWorkspace = "packages/coding-agent";
   npmFlags = [ "--legacy-peer-deps" ];
   makeCacheWritable = true;
